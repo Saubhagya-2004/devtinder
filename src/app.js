05ff3a5131a074c47421c48cd1233f439176382f
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-04-05 07:30:55 +0530

    dynamic data -in sign up Api

--- src/app.js
+++ src/app.js
@@ -3,16 +3,14 @@ const connectDB = require('./config/database');
 const app = express(); // Create an express application
 //first connect to the database then listen to the server
 const User = require('./models/user')
+//use express middle-ware
+app.use(express.json())
 app.post('/signup',async(req,res)=>{
     //return a promise so use async
-    //creating a new instance of usermodel
-    const user = new User({
-        firstName:'Dhoni',
-        lastName:'singh',
-        email:'ms@gmail.com',
-        password:'ms7'
-
-    })
+    //creating a new instance of usermodel dynamic
+    const user = new User(req.body)
+    console.log(user);
+    
     //always wrap it inside try catch
     try{
 
