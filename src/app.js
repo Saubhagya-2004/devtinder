cbd552e3aa4eb5ccca824604864b461ab2ac934c
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2025-04-27 23:31:57 +0530

    add ,update delete user model

--- src/app.js
+++ src/app.js
@@ -18,27 +18,77 @@ app.use("/admin", admin, (req, res) => {
 app.get("/user", userAuth, (req, res) => {
   res.send({ firstName: "Saubhagya", LastName: "Baliarsingh" });
 });
-app.post("/signup", async (req, res) => {
-  //return a promise so use async
-  //creating a new instance of usermodel dynamic
-  const user = new User(req.body);
-  console.log(user);
-  
+
+//add data into database
+app.post('/signup',async(req,res)=>{
+const user = new User(req.body);
+console.log(req.body);
+try{
+  await user.save();
+  res.send('user added successfully');
+}
+catch(err){
+  res.status(400).send('Error adding the user');
+}
+});
+
+//get data by email
+app.get('/useremail',async(req,res)=>{
+  const userEmail = req.body.email;
   try{
-    await user.save();
-  res.send('user added sucessfully');
+    const user = await User.find({email:userEmail});
+    if(user.length==0){
+      return res.status(404).send('User not found');
+    }
+    res.send(user);
   }
-  catch(err){
-    res.status(400).send('Failed creating the user');
+  catch{
+    res.status(400).send('Error finding the user');
   }
-});
-app.delete("/user", async (req, res) => {
+})
+
+//get data by id
+app.get('/userid',async(req,res)=>{
+  const userid = req.body._id;
+  try{
+    const user= await User.findById({_id:userid});
+    res.send(user);
+  }
+  catch{
+    res.status(400).send('Error finding the user');
+  }
+})
+
+//get all data
+app.get('/feed',async(req,res)=>{
+  try{
+    const user = await User.find({});
+    res.send(user);
+  }
+  catch{
+    res.status(400).send('Error not get Data');
+  }
+})
+
+//delete user by name
+app.delete("/userdel", async (req, res) => {
+  const username = req.body.firstName; 
+
   try {
-    res.send("user deleted sucessfully");
-  } catch {
-    res.status(400).send("Error deleting the message");
+    const user = await User.findOneAndDelete({firstName:username}); 
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+   
+    res.send('User deleted successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Error deleting the user");
   }
 });
+
+
+
 connectDB()
   .then(() => {
     console.log("Database connection sucessfully...");
