adbe9ffd92ad17dbab970d89f0c646626f845dfd
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2025-06-30 18:07:20 +0530

    get  feed

--- src/Routes/UserRequest.js
+++ src/Routes/UserRequest.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const UserRequest = express.Router();
 const Connectionreq = require("../models/connectionreq");
-
+const User = require("../models/user");
 //get all the pending connection requests
 UserRequest.get("/user/requests/recived", userAuth, async (req, res) => {
   try {
@@ -63,4 +63,39 @@ UserRequest.get("/user/connections", userAuth, async (req, res) => {
     });
   }
 });
+
+//Get all data feed
+UserRequest.get("/feed", userAuth, async (req, res) => {
+  try {
+    const LoggedinUser = req.user;
+    const connectionreq = await Connectionreq.find({
+      $or: [{ senderId: LoggedinUser._id }, { ReciverId: LoggedinUser._id }],
+    }).select("senderId ReciverId");
+    //collect not sending id
+    const hiddenuserId = new Set();
+    //filtering that value
+    connectionreq.forEach((req) => {
+      hiddenuserId.add(req.senderId.toString()),
+        hiddenuserId.add(req.ReciverId.toString());
+    });
+    // console.log(hiddenuserId);
+
+    const user = await User.find({
+      $and: [
+        //find in user db which user is not present in hiddenuserId send that
+        { _id: { $nin: Array.from(hiddenuserId) } },
+        //and not send loggedin user profile
+        { _id: { $ne: LoggedinUser._id } },
+      ],
+    }).select("firstName lastName age gender Bio skills language profile");
+    // console.log(user + "not send req");
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).json({
+      message: "ERROR" + err.message,
+    });
+  }
+});
+
 module.exports = UserRequest;
