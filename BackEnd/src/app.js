91182fae2ec5a2ad28d4378b31a053253dd8ed79
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-02-28 21:43:47 +0530

    Fix CORS error for netlify

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -37,7 +37,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://developertindor.netlify.app"
+  "https://developertindor.netlify.app",
+  "https://silly-taffy-3413a1.netlify.app"
 ];
 
 app.use(cors({
