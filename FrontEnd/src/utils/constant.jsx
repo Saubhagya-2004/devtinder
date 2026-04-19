074e19bc33d7b8c88cd53dac1668c4a2358d24ce
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-04-20 00:40:31 +0530

    dev

--- FrontEnd/src/utils/constant.jsx
+++ FrontEnd/src/utils/constant.jsx
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "https://devtinder-ltnl.onrender.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://devtinder-ltnl.onrender.com";
